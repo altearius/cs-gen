97b3338c29168b88608b3a03fd49d85891ef257b
Chris Nielsen <nielsen.chris@gmail.com>
2023-07-31 11:11:42 -0400

    Link fields are never given null properties.

--- src/transform/json-schema/LinkFieldDefinition.ts
+++ src/transform/json-schema/LinkFieldDefinition.ts
@@ -3,5 +3,6 @@ import S from 'fluent-json-schema';
 export default function LinkDefinition() {
 	return S.object()
 		.prop('title', S.string())
-		.prop('href', S.string().format('uri'));
+		.prop('href', S.string().format('uri'))
+		.required(['title', 'href']);
 }
