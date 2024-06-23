73aaa07f64d36b7d9750eea67ad106b23a16f055
Chris Nielsen <nielsen.chris@gmail.com>
2024-06-23 19:40:51 -0400

    Schemas are no longer used at run time.

--- scripts/build.js
+++ scripts/build.js
@@ -3,11 +3,10 @@
 import * as generate from './lib/generate-types.js';
 import { generateValidation } from './lib/generate-validator.js';
 import compileTs from './lib/compile-ts.js';
-import copySchema from './lib/copy-schema.js';
 import setExec from './lib/set-exec.js';
 
 await Promise.all([generateValidation(), generate.schemas()]);
 
 compileTs();
 
-await Promise.all([copySchema(), setExec()]);
+await setExec();
